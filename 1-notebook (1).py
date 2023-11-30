abfcc243fa746041748f08e1a8e9ccecdf70d70a
mukulChoudhary12 <152527635+mukulChoudhary12@users.noreply.github.com>
2023-11-30 23:32:08 +0530

    Add files via upload

---
+++ 1-notebook (1).py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction to Data Science
+# 
+# ## Welcome to the Notebook!
+# 
+# In this notebook, we'll explore the fascinating world of data science. Let's get started!
+# 
+# ## Data Science Languages
+# 
+# - Python
+# - R
+# - SQL
+# - Julia
+# - Scala
+# 
+# ## Data Science Libraries
+# 
+# - NumPy
+# - Pandas
+# - Matplotlib
+# - Seaborn
+# - Scikit-learn
+# - TensorFlow
+# - PyTorch
+# 
+# ## Data Science Tools
+# 
+# | Category       | Tools                |
+# | -------------- | -------------------- |
+# | Programming    | Python, R, SQL, Julia|
+# | Data Analysis  | Pandas, NumPy        |
+# | Visualization  | Matplotlib, Seaborn  |
+# | Machine Learning| Scikit-learn, TensorFlow, PyTorch |
+# 
+# ## Arithmetic Expression Examples
+# 
+# In this section, we'll explore various arithmetic expressions to reinforce fundamental mathematical operations in data science.
+# 
+# - Addition: \( a + b \)
+# - Subtraction: \( a - b \)
+# - Multiplication: \( a \times b \)
+# - Division: \( \frac{a}{b} \)
+# 
+# 
---
+++ Untitled3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Objectives
+# 
+# In this exercise, we aim to:
+# 
+# 1. Understand the basics of data science languages and libraries.
+# 2. Explore common tools used in the field of data science.
+# 3. Practice converting units, specifically minutes to hours, using Python.
+# 4. Reinforce fundamental arithmetic expressions for data manipulation.
+# 
+# ## Author
+# 
+# - [MUKUL CHOUDHARY]
+# 
