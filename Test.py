a710de569886a4a634abffde6c75a63d9bbe4bed
mukulChoudhary12 <152527635+mukulChoudhary12@users.noreply.github.com>
2023-11-30 23:28:57 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+(4*5)+6
+
+
+# In[3]:
+
+
+# Exercise 9 - Convert Minutes to Hours
+
+def convert_minutes_to_hours(minutes):
+    hours = minutes / 60
+    return hours
+
+minutes_value = 120
+hours_result = convert_minutes_to_hours(minutes_value)
+
+print(f"{minutes_value} minutes is equal to {hours_result} hours.")
+
+
+# In[ ]:
+
+
+
+
