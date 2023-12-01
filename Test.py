0bae0b776598e36bb648120c28d92c8705888fc8
mukulChoudhary12 <152527635+mukulChoudhary12@users.noreply.github.com>
2023-12-01 11:12:30 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
-(4*5)+6
-
-
-# In[3]:
-
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
 
 # Exercise 9 - Convert Minutes to Hours
 
-def convert_minutes_to_hours(minutes):
-    hours = minutes / 60
-    return hours
-
-minutes_value = 120
-hours_result = convert_minutes_to_hours(minutes_value)
-
-print(f"{minutes_value} minutes is equal to {hours_result} hours.")
-
-
-# In[ ]:
-
-
-
-
+minutes=200
+# below is the formula to convert minutes into hours
+hours = minutes / 60
+hours
