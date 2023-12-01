01166d6bc0db1718c27f77aa18b25ff93cbff50a
mukulChoudhary12 <152527635+mukulChoudhary12@users.noreply.github.com>
2023-12-01 11:10:58 +0530

    Update 1-notebook (1).py

--- 1-notebook (1).py
+++ 1-notebook (1).py
@@ -1,46 +1,92 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Introduction to Data Science
-# 
-# ## Welcome to the Notebook!
-# 
-# In this notebook, we'll explore the fascinating world of data science. Let's get started!
-# 
-# ## Data Science Languages
-# 
-# - Python
-# - R
-# - SQL
-# - Julia
-# - Scala
-# 
-# ## Data Science Libraries
-# 
-# - NumPy
-# - Pandas
-# - Matplotlib
-# - Seaborn
-# - Scikit-learn
-# - TensorFlow
-# - PyTorch
-# 
-# ## Data Science Tools
-# 
-# | Category       | Tools                |
-# | -------------- | -------------------- |
-# | Programming    | Python, R, SQL, Julia|
-# | Data Analysis  | Pandas, NumPy        |
-# | Visualization  | Matplotlib, Seaborn  |
-# | Machine Learning| Scikit-learn, TensorFlow, PyTorch |
-# 
-# ## Arithmetic Expression Examples
-# 
-# In this section, we'll explore various arithmetic expressions to reinforce fundamental mathematical operations in data science.
-# 
-# - Addition: \( a + b \)
-# - Subtraction: \( a - b \)
-# - Multiplication: \( a \times b \)
-# - Division: \( \frac{a}{b} \)
-# 
-# 
+# Introduction to Data Science
+
+## Welcome to the Notebook!
+
+In this notebook, we'll explore the fascinating world of data science. Let's get started!
+
+## Data Science Languages
+
+- Python
+- R
+- SQL
+- Julia
+- Scala
+- Java
+- C++
+- Javascript
+- PHP
+- Go
+- Ruby
+- Visual Basic
+
+## Data Science Libraries
+
+- NumPy
+- Pandas
+- Matplotlib
+- Seaborn
+- Scikit-learn
+- TensorFlow
+- PyTorch
+- Keras
+- Apache Spark
+- Vegas
+- Big DL
+- Ggplot2
+
+## Data Science Tools
+
+| Data Science Tools |
+|--------------------|
+| Jupyter Notebooks  |
+| RStudio            |
+| VSCode             |
+| Spyder             |
+| Zeppelin           |
+| Anaconda           |
+
+
+## Arithmetic Expression Examples
+
+Below are a few examples of evaluating arithmetic expressions in Python
+
+- Addition: \( a + b \)
+- Subtraction: \( a - b \)
+- Multiplication: \( a \times b \)
+- Division: \( \frac{a}{b} \)
+
+    
+## Introduction
+
+(This is list of our objectives)
+
+## Objectives:
+
+**1. List popular languages for Data Science:**
+   - Python
+   - R
+   - Julia
+**2. Understand key libraries for machine learning:**
+   - Scikit-learn
+   - TensorFlow
+   - PyTorch
+**3. Explore common data visualization tools:**
+   - Matplotlib
+   - Seaborn
+   - Plotly
+**4. Learn about Jupyter Notebooks and their features:**
+   - Interactive computing
+   - Support for multiple programming languages
+   - Data visualizations
+**5. Gain insights into data governance and stewardship:**
+   - Importance of data governance
+   - Role of data stewardship
+   - Data quality management
+
+
+## Author
+
+- [MUKUL CHOUDHARY]
